622efa02059458a031e42498cf72453c6f4d817a
Kirubanand <prdkiruba@gmail.com>
2021-03-09 22:43:27 +0530

    Files added

---
+++ app/src/main/java/com/example/materialcheckbox/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.materialcheckbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(this , "$isChecked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
